b527f9b682807060742b2b238a2cf19b16253853
YamabeKouta <89598222+YamabeKouta@users.noreply.github.com>
2021-09-05 23:19:15 +0900

    検索機能

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,6 +1,6 @@
 class TweetsController < ApplicationController
   before_action :set_tweet, only: [:edit, :show]
-  before_action :move_to_index,except: [:show, :index]
+  before_action :move_to_index, except: [:index, :show, :search]
 
   def index
     @tweets = Tweet.includes(:user).order("created_at DESC")
@@ -24,6 +24,9 @@ class TweetsController < ApplicationController
   def show
     @comment = Comment.new
     @comments = @tweet.comments.includes(:user)
+  end
+  def search
+    @tweets = Tweet.search(params[:keyword])
   end
 
   private
