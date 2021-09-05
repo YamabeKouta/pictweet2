a22393a865472f1d60dab37975638a3bddb7698e
YamabeKouta <89598222+YamabeKouta@users.noreply.github.com>
2021-09-05 09:24:55 +0900

    改行

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -31,7 +31,6 @@ class TweetsController < ApplicationController
   def set_tweet
     @tweet = Tweet.find(params[:id])
   end
-
   def move_to_index
     unless user_signed_in?
       redirect_to action: :index
